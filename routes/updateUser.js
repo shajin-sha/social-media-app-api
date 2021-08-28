f6468a2288eab73de96a050700753b5687fe0f43
shajin-sha <shajin.sha10@gmail.com>
2021-08-28 16:15:56 +0530

    user dp have some changes

--- routes/updateUser.js
+++ routes/updateUser.js
@@ -1,56 +1,11 @@
 var express = require('express');
 var router = express.Router();
 var MongoClient = require("mongodb").MongoClient
-var multer = require("multer")
-
-
-
-let date = new Date()
-
-const dateNow = date.getTime()
-
-
-
-const DpFIleName = dateNow + "--user"
-
-
-const storage = multer.diskStorage({
-    destination: function (request, file, callback) {
-        // node.js express public dir 
-        // storing static file at this dir 
-        // and access this from react-app
-        callback(null, "./public/images/profile")
-
-    },
-
-
-    filename: function (request, file, callback,) {
-        callback(null, DpFIleName + file.originalname);
-    }
-})
-
-
-const upload = multer({
-    storage: storage,
-    limits: {
-        fieldSize: 1024 * 1024 * 3
-    }
-})
 
 
 
 
-
-
-
-
-
-
-
-
-
-
-router.post("/", upload.single("Dp"), (req, res, next) => {
+router.post("/", (req, res, next) => {
     const uri = 'mongodb+srv://shajin:shajin1530.@cluster1.umyhu.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
     MongoClient.connect(uri, (err, client) => {
         if (err) {
@@ -62,7 +17,7 @@ router.post("/", upload.single("Dp"), (req, res, next) => {
                 Name: req.body.Name,
                 userName: req.body.userName,
                 Bio: req.body.Bio,
-                Dp: DpFIleName + req.body.DpFIleName,
+                Dp:req.body.DpUrl,
                 profileUpdated: req.body.profileUpdated,
             }
             client.db("feed_app").collection("users").updateOne({ userName: data.userName }, { $set: { ...data } }, (err) => {
@@ -73,7 +28,7 @@ router.post("/", upload.single("Dp"), (req, res, next) => {
                         })
                 }
                 else {
-                    res.json({ dp: DpFIleName + req.body.DpFIleName })
+                    res.json({ dp: req.body.DPUrl})
                 }
             })
         }
